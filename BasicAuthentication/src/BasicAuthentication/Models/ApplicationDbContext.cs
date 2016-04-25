2b30edf5d4b7ee5155657149b944c39da7682f7c
Taylor <taylorgrohs@gmail.com>
2016-04-25 10:36:50 -0700

    Add todo list with user auth.

---
+++ BasicAuthentication/src/BasicAuthentication/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace BasicAuthentication.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
